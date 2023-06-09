5d524689fce7bd250aa7025b4afbb62babb98466
Sam Tubbax <sam@shenanigans.be>
2023-06-09 10:45:23 +0200

    Favicon and OG image, like a real website n shit

--- index.php
+++ index.php
@@ -10,6 +10,18 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,600;1,300&family=Staatliches&display=swap" rel="stylesheet">
+
+    <meta property="og:image" content="<?= url('og.png') ?>"/>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1a2266">
+    <meta name="msapplication-TileColor" content="#fdfcf8">
+    <meta name="theme-color" content="#ffffff">
+
+
 </head>
 <body>
     <div class="container">

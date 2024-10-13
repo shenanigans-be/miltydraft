5b2f792448fef2b16092cdef91ff16d2ca8990c7
Sam Tubbax <sam@shenanigans.be>
2024-10-13 09:33:51 +0200

    Fix

--- pick.php
+++ pick.php
@@ -14,7 +14,7 @@ $is_admin = $draft->isAdminPass(get('admin'));
 if ($draft == null) return_error('draft not found');
 if ($player != $draft->currentPlayer() && !$is_admin) return_error('Not your turn!');
 
-Not enforcing this here yet because it would break for older drafts
+// Not enforcing this here yet because it would break for older drafts
 if (!$is_admin && !$draft->isPlayerSecret($player, get('secret'))) return_error('You are not allowed to do this!');
 
 if ($index != count($draft->log())) {

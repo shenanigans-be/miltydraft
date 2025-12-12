142d74487755a60351a0be2242b01f234db50cfe
Sam Tubbax <sam@shenanigans.be>
2025-12-12 18:58:15 +0100

    Fix paratest command

--- app/Game/PlanetTest.php
+++ app/Game/PlanetTest.php
@@ -158,22 +158,4 @@ class PlanetTest extends TestCase
         $this->assertSame($expectedTechSpecialties, $planet->specialties);
         $this->assertSame($expectedLegendary, $planet->legendary);
     }
-
-    // @todo maybe move this to a JsonValidationTest
-    #[Test]
-    public function allPlanetsInJsonAreValid() {
-        $tileData = json_decode(file_get_contents('data/tiles.json'), true);
-        $planetJsonData = [];
-        foreach($tileData as $t) {
-            foreach($t['planets'] as $p) {
-                $planetJsonData[] = $p;
-            }
-        }
-
-        $planets = array_map(fn (array $data) => Planet::fromJsonData($data), $planetJsonData);
-
-        $this->assertCount(count($planetJsonData), $planets);
-    }
-
-
 }

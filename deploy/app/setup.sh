b53111bc9b68e075e763946b3463f9469eb0c1ed
Sam Tubbax <sam@shenanigans.be>
2026-02-28 15:17:44 +0100

    Simplify docker setup

--- deploy/app/setup.sh
+++ deploy/app/setup.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env sh
 
 # Add a non-root user we can use to run the app
-addgroup -g 1000 -S app \
-  && adduser -u 1000 -S app -G app \
-  && chown app /code
 
-# Install container dependencies
-apk --update --no-cache add \
-  autoconf \
-  curl \
-  cyrus-sasl-dev \
-  g++ \
-  git \
-  libtool \
-  make \
-  linux-headers
